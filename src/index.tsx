782da62e5d61344a1862998ef4d8fa074338507a
Kumar Abhirup <kumar@iqubex.com>
2019-09-01 22:46:05 +0530

    📦 NEW: ESLint and Prettier!

--- src/index.tsx
+++ src/index.tsx
@@ -9,15 +9,8 @@ import styles from './styles.css'
 export type Props = { text: string }
 
 export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
+  render(): React.ReactNode {
+    const { text } = this.props
+    return <div className={styles.test}>Example Component: {text}</div>
   }
 }
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
-import ExampleComponent from './'
+import ExampleComponent from './index'
 
 describe('ExampleComponent', () => {
   it('is truthy', () => {
